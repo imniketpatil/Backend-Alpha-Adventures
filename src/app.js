7e1caab78abd6a82e0189722278c46714d142964
Niket Patil <patilniket41@gmail.com>
2024-08-27 16:31:42 +0530

    Done

--- src/app.js
+++ src/app.js
@@ -7,8 +7,8 @@ const app = express();
 
 // Define allowed origins
 const allowedOrigins = [
-  "http://localhost:5173/alpha-adventures",
-  "http://localhost:5174",
+  "http://localhost:5173/alpha-adventures/",
+  "http://localhost:5174/",
 ];
 
 // Configure CORS options
