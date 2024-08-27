413e1117e475112a2ddc6b2b55328963b5f15522
Niket Patil <patilniket41@gmail.com>
2024-08-27 14:29:13 +0530

    done

--- src/app.js
+++ src/app.js
@@ -4,11 +4,13 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
+// Define allowed origins
 const allowedOrigins = [
   "https://alpha-adventures-client.onrender.com",
   "https://alpha-adventures-admin.onrender.com",
 ];
 
+// Configure CORS options
 const corsOptions = {
   origin: (origin, callback) => {
     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
@@ -20,38 +22,33 @@ const corsOptions = {
   },
   methods: ["GET", "POST", "PATCH", "DELETE"], // Specify allowed HTTP methods
   credentials: true, // Allow cookies and other credentials to be sent
-  allowedHeaders: [
-    "Content-Type",
-    "Authorization",
-    "Access-Control-Allow-Origin",
-  ],
+  allowedHeaders: ["Content-Type", "Authorization"],
 };
 
+// Apply CORS middleware
 app.use(cors(corsOptions));
 
+// Middleware
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
-//routes
+// Import routes
 import userRouter from "./routes/user.routes.js";
 import trekGuideRouter from "./routes/trekguide.routes.js";
 import trekTypeRouter from "./routes/trektype.routes.js";
 import trekRouter from "./routes/trek.routes.js";
 import testimonialRouter from "./routes/testimonial.routes.js";
 
-//routes declearation
+// Route declarations
 app.use("/api/v1/users", userRouter);
-
 app.use("/api/v1/trekguide", trekGuideRouter);
-
 app.use("/api/v1/trektype", trekTypeRouter);
-
 app.use("/api/v1/trek", trekRouter);
-
 app.use("/api/v1/testimonial", testimonialRouter);
 
+// Test route
 app.get("/connect", (req, res) => {
   res.send("Trek types data");
 });
